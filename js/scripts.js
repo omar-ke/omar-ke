723fc9ad4fb8c4312c82b9ee0312f86be4a9698f
omar-ke <1010701@schoolsnet.act.edu.au>
2019-04-01 15:19:38 +1100

    enviro js

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,38 @@ function pinCheck() {
     }
     document.getElementById("feedback").innerHTML = text;
 }
+
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("bulbon")) {
+        image.src = "images/pic_bulboff.gif";
+    } else {
+        image.src = "images/pic_bulbon.gif";
+    }
+}
+
+function check_pin_code() {
+    var pin_entered, text;
+    //Get the value of input field with id="pin_code"
+    pin_entered = document.getElementById("pin_code").value;
+    // Check to see if pin_entered is correct
+    if (pin_entered == "1234") {
+        text = '<div class="bs-example">\n' +
+            '<div class="row">\n' +
+            '<div class="col-md-6"><img src="images/pic_bulbon.gif" alt="" width="100" height="180" id="main_power"></div>\n' +
+            '<div class="col-md-6"><img id="secondary_power" onclick="changeImage()" src="images/pic_bulboff.gif" width="100" height="180"></div>\n' +
+            '</div>\n'
+    } else {
+        text = "Input Not valid";
+    }
+    document.getElementById("power_usage").innerHTML = text;
+}
+
+function changeImage() {
+    var image = document.getElementById('secondary_power');
+    if (image.src.match("bulbon")) {
+        image.src = "images/pic_bulboff.gif";
+    } else {
+        image.src = "images/pic_bulbon.gif";
+    }
+}
