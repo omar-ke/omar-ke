6a8b87a002a7439864aa4c9de77b11c13c786c3c
omar-ke <1010701@schoolsnet.act.edu.au>
2019-05-06 15:30:55 +1000

    dca

--- js/dca.js
+++ js/dca.js
@@ -1,15 +1,29 @@
 function Heatmap() {
-    var phone = [Math.floor(Math.random() * 30)];
+    var phone = [Math.floor(Math.random() * 3)];
+    var tv = [Math.floor(Math.random() * 3)];
 
 
-    if (phone <= 10) {
-        document.getElementById('phone').innerHTML = "Poor";
+    if (phone <= 1) {
+        document.getElementById('phone').innerHTML = "Poorly";
         document.getElementById('phone').style.color = "red";
-    } else if (phone <= 20) {
+    } else if (phone <= 2) {
         document.getElementById('phone').innerHTML = "Okay";
         document.getElementById('phone').style.color = "orange";
-    } else if (phone <= 30) {
+    } else if (phone <= 3) {
         document.getElementById('phone').innerHTML = "Good";
         document.getElementById('phone').style.color = "green";
     }
+    if (tv <= 1) {
+        document.getElementById('tv').innerHTML = "Poorly";
+        document.getElementById('tv').style.color = "red";
+    } else if (tv <= 2) {
+        document.getElementById('tv').innerHTML = "Okay";
+        document.getElementById('tv').style.color = "orange";
+    } else if (tv <= 3) {
+        document.getElementById('tv').innerHTML = "Good";
+        document.getElementById('tv').style.color = "green";
+    }
+
+
+    setTimeout('location.reload()', 5000);
 }
