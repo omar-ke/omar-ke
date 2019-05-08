6ceb204dbf3db5a4fc1d68c6e5bdc4c29d69f776
omar-ke <1010701@schoolsnet.act.edu.au>
2019-05-08 12:32:09 +1000

    starting on dhs

--- js/dhs.js
+++ js/dhs.js
@@ -1,16 +1,10 @@
 /* Human Services JavaScript */
-const test = ["123", "321"];
-const rand = Math.floor(Math.random() * 2);
-
-
-/* Generates the random user data which is seen on the Human Services page. */
 function items() {
+    var test = ["123", "321"];
+    var rand = [Math.floor(Math.random() * 2)];
+
+
+
     document.getElementById('test').value = test[rand];
 
-
-/* Tells the user they have updated their details and refreshes page after 5 seconds. */
-function userDetailsSubmit() {
-    setTimeout('location.reload()',5000);
-    document.getElementById('updateSuccess').style.display = "block";
-    document.getElementById('updateSuccess').innerHTML = "Your details have been updated! This page will refresh in 5 seconds.";
 }
