39047582066d1b761953fb79edb0c0fc16781a88
omar-ke <1010701@schoolsnet.act.edu.au>
2019-05-24 10:07:40 +1000

    Everything.

--- js/scripts.js
+++ js/scripts.js
@@ -54,7 +54,7 @@ function validateElectionForm() {
         document.getElementById('candidateSuccess').innerHTML = "Thank you for voting! Your form has been submitted to the AEC! This page will refresh in 5 seconds.";
         document.getElementById('candidateError').style.display = "none";
         document.getElementById('candidateError').innerHTML = "";
-        setTimeout('location.reload()',5000)
+        setTimeout('location.reload()',51000)
     } else {
         document.getElementById('candidateSuccess').style.display = "none";
         document.getElementById('candidateSuccess').innerHTML = "";
